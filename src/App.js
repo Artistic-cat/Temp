19122637ba55aeb3d0a2d9d600e5ec0977889be2
Mohammed Rifas S <rifas2475@outlook.com>
2020-10-28 21:49:42 +0530

    App JS adding bootstrap
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Login from './Views/Login/Login';
 import Home from "./Views/Home/Home"
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
 
