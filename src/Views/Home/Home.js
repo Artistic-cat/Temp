dbd8bfe224050f6436423f0c30bfef61ccbd7e3e
Mohammed Rifas S <rifas2475@outlook.com>
2020-10-28 21:50:26 +0530

    Home Page JS
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -4,14 +4,39 @@ import "./Home.css"
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
 import Sidebar from "../../Components/Sidebar/Sidebar";
+import {Container} from "react-bootstrap";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import {MdPhonelinkSetup} from "react-icons/md";
+import {FiSettings, FiUsers} from "react-icons/fi";
+import {BsCheckCircle} from "react-icons/bs";
 
 function Home() {
     return (
         <div>
+            <Sidebar/>
             <Header/>
-            <div className="home" id="home">
-                <Sidebar/>
-            </div>
+            <Container>
+                <Row>
+                    <Col>
+                        <MdPhonelinkSetup style={{fontSize: '8vh'}} className="circle-inner-text mb-2"/>
+                        <p>Product Setup</p>
+                    </Col>
+                    <Col>
+                        <FiSettings style={{fontSize: '8vh'}} className="circle-inner-text mb-2"/>
+                        <p>Configurations</p>
+                    </Col>
+                    <Col>
+                        <BsCheckCircle style={{fontSize: '8vh'}} className="circle-inner-text mb-2"/>
+                        <p>Approvals</p>
+                    </Col>
+                    <Col>
+                        <FiUsers style={{fontSize: '8vh'}} className="circle-inner-text mb-2"/>
+                        <p>User Management</p>
+                    </Col>
+                </Row>
+            </Container>
+
             <Footer/>
         </div>
     )
