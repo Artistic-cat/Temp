f56dfcd968c1abbb12235e42cf73a664747cb801
Mohammed Rifas S <rifas2475@outlook.com>
2020-10-29 21:16:39 +0530

    Login wrap * change
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- src/Views/Login/Login.js
+++ src/Views/Login/Login.js
@@ -1,25 +1,28 @@
 import React, {useState} from 'react';
 import './Login.css';
+
 //import {Link} from 'react-router-dom';
 
 function Login() {
 
-    const [email, setEmail]= useState('');
-    const [password, setPassword]=useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     const logIn = e => {
         e.preventDefault();
         //add node functionality here
     }
 
-    return(
+    return (
         <div className='login'>
             <div className='login-block'>
                 <div className='login-logo'>LOGO</div>
                 <div className='login-form'>
-                    <input className='input' type='text' placeholder='Username/Email ID' value={email} onChange={e => setEmail(e.target.value)} />
-                    <input className='input' type='password' placeholder='Password' value={password} onChange={e=> setPassword(e.target.value)} />
-                    <button className='login-button' type='submit' onClick={logIn} >Log In</button>
+                    <input className='input' type='text' placeholder='Username/Email ID' value={email}
+                           onChange={e => setEmail(e.target.value)}/>
+                    <input className='input' type='password' placeholder='Password' value={password}
+                           onChange={e => setPassword(e.target.value)}/>
+                    <button className='login-button' type='submit' onClick={logIn}>Log In</button>
                 </div>
             </div>
         </div>
