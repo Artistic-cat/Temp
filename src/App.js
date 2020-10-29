90eaf9f8dee53b330bf3275bca979140e6dda36c
Mohammed Rifas S <rifas2475@outlook.com>
2020-10-29 21:14:45 +0530

    Refactor
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- src/App.js
+++ src/App.js
@@ -5,23 +5,27 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
+import FileDropBox from "./Components/FlieDropBox/FileDropBox";
 
 function App() {
-  return (
-      <Router>
-        <div className="App">
-          <Switch>
-            <Route path="/home">
-              <Home/>
-          </Route>
-          <Route path="/">
-            <Login />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-      
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Switch>
+                    <Route path="/home">
+                        <Home/>
+                    </Route>
+                    <Route path="/files">
+                        <FileDropBox/>
+                    </Route>
+                    <Route path="/">
+                        <Login/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+
+    );
 }
 
 export default App;
