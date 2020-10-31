408e2d6bbb7f714fd852d2eb44c7e4e8af7ff3dd
Mohammed Rifas S <rifas2475@outlook.com>
2020-10-31 21:25:55 +0530

    recently used implementation
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

---
+++ src/Components/RecentlyUsed/RecentlyUsed.js
@@ -0,0 +1,48 @@
+import React, {Component} from "react";
+import localstorage from "local-storage";
+
+import Row from "react-bootstrap/Row";
+
+class RecentlyUsed extends Component {
+    constructor(props) {
+        super(props);
+        let pages = JSON.parse(localStorage.getItem("page"));
+        this.state = {
+            pages: pages
+        };
+    }
+
+    setPath(title, path) {
+        let current = localStorage.getItem(title);
+        const pages = localStorage.getItem("page")
+        if (!current) {
+            localstorage(title, path);
+            const page = JSON.parse(pages);
+            //localstorage("page", "("+title+","+JSON.parse(current)+")");
+            const a = "<Col> \
+                              <button className=\"btn\" onClick=\"/" + title + "\"> \
+                                  <MdPhonelinkSetup style=\"fontSize: '8vh' className=\"home-circle-inner-text mb-2\"/> \
+                                  <p>+title+</p> \
+                              </button> \
+                           </Col>"
+            if (!pages)
+                localstorage("page", a)
+            else
+                localstorage("page", a + page);
+
+            //localstorage.removeItem(title);
+        }
+    }
+
+
+    render() {
+        return (
+            <Row className="home-nav-buttons"
+                 dangerouslySetInnerHTML={{
+                     __html: this.state.pages
+                 }}></Row>
+        )
+    }
+}
+
+export default RecentlyUsed;
