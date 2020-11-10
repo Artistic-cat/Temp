e4849a67ed5847b16bee75b2c28578f59da55a2f
Mohammed Rifas S <rifas2475@outlook.com>
2020-11-10 19:07:02 +0530

    Refactor
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- src/App.js
+++ src/App.js
@@ -5,35 +5,39 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
+import About from "./Views/About/About";
+import Setup from "./Views/Setup/Setup";
+import FileDropBox from "./Components/FlieDropBox/FileDropBox";
+import Reset from "./Views/Login/Reset";
 
 function App() {
-  return (
-      <Router>
-        <div className="App">
-          <Switch>
-            <Route path="/home">
-              <Home/>
-            </Route>
-            <Route path = "/About">
-              <About/>
-            </Route>
-            <Route path="/Setup">
-                <Setup/>
-            </Route>
-            <Route path="/reset">
-                  <Reset />
-            </Route>
-            <Route path="/files">
-                <FileDropBox/>
-            </Route>
-            <Route path="/">
-                <Login />
-            </Route>
-        </Switch>
-      </div>
-    </Router>
-      
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Switch>
+                    <Route path="/home">
+                        <Home/>
+                    </Route>
+                    <Route path="/About">
+                        <About/>
+                    </Route>
+                    <Route path="/Setup">
+                        <Setup/>
+                    </Route>
+                    <Route path="/reset">
+                        <Reset/>
+                    </Route>
+                    <Route path="/files">
+                        <FileDropBox/>
+                    </Route>
+                    <Route path="/">
+                        <Login/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+
+    );
 }
 
 export default App;
