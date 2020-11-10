52bce3465284d61644b38f3becc7ccebc20c5e69
Mohammed Rifas S <rifas2475@outlook.com>
2020-11-10 20:25:29 +0530

    Recently Used
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- src/Components/RecentlyUsed/RecentlyUsed.js
+++ src/Components/RecentlyUsed/RecentlyUsed.js
@@ -1,41 +1,34 @@
 import React, {Component} from "react";
 import localstorage from "local-storage";
 
-import Row from "react-bootstrap/Row";
+import "./RecentlyUsed.css";
 
 class RecentlyUsed extends Component {
     constructor(props) {
         super(props);
-        let pages = JSON.parse(localStorage.getItem("page"));
+        let pages = new Map(JSON.parse(localStorage.getItem("pages")));
         this.state = {
             pages: pages
         };
     }
 
     setPath(title, path) {
-        let current = localStorage.getItem(title);
-        const pages = localStorage.getItem("page")
-        if (!current) {
-            localstorage(title, path);
-            const page = JSON.parse(pages);
-            //localstorage("page", "("+title+","+JSON.parse(current)+")");
-            const a = "<Col><button className=\"btn\" onClick=\"/" + title + "\"> <MdPhonelinkSetup style=\"fontSize: '8vh' className=\"home-circle-inner-text mb-2\"/> <p>+title+</p><button></Col>"
-            if (!pages)
-                localstorage("page", a)
-            else
-                localstorage("page", a + page);
-
-            //localstorage.removeItem(title);
-        }
+        this.state.pages.set(title, path);
+        localstorage("pages", Array.from(this.state.pages.entries()));
     }
 
 
     render() {
         return (
-            <Row className="home-nav-buttons"
-                 dangerouslySetInnerHTML={{
-                     __html: this.state.pages
-                 }}/>
+            <div className="recentlyUsed-container">
+                {
+                    [...this.state.pages].map((entry) => {
+                        let title = entry[0]
+                        let path = entry[1]
+                        return <p><a href={path}>{title}</a></p>
+                    })
+                }
+            </div>
         )
     }
 }
