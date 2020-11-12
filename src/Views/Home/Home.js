c4c9970fa9570fe24c7964f4e6c378aaca115fbf
Mohammed Rifas S <rifas2475@outlook.com>
2020-11-12 20:27:33 +0530

    Chars added
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -12,7 +12,7 @@ import {FiSettings, FiUsers} from "react-icons/fi";
 import {BsCheckCircle} from "react-icons/bs";
 import RecentlyUsed from "../../Components/RecentlyUsed/RecentlyUsed";
 import {BiAnalyse, SiLivejournal, SiLogstash} from "react-icons/all";
-import {Line} from "react-chartjs-2";
+import {Doughnut, Line, Scatter} from "react-chartjs-2";
 
 class Home extends Component {
     constructor(props) {
@@ -40,6 +40,151 @@ class Home extends Component {
                 data: [10, 0, 50, 2, 20, 15, 5],
             }]
         }
+
+        // line chart starts
+
+        const lineData = {
+            labels: ['1', '2', '3', '4', '5', '6'],
+            datasets: [
+                {
+                    label: 'Crash Logs',
+                    data: [12, 19, 3, 5, 2, 3],
+                    fill: false,
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgba(255, 99, 132, 0.2)',
+                },
+            ],
+        }
+
+        const lineOptions = {
+            scales: {
+                yAxes: [
+                    {
+                        ticks: {
+                            beginAtZero: true,
+                        },
+                    },
+                ],
+            },
+        }
+
+        // Line chart ends
+
+        // multi line starts
+        const multiLineData = {
+            labels: ['1', '2', '3', '4', '5', '6'],
+            datasets: [
+                {
+                    label: 'Product 1',
+                    data: [12, 19, 3, 5, 2, 3],
+                    fill: false,
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgba(255, 99, 132, 0.2)',
+                    yAxisID: 'y-axis-1',
+                },
+                {
+                    label: 'Product 2',
+                    data: [1, 2, 1, 1, 2, 2],
+                    fill: false,
+                    backgroundColor: 'rgb(54, 162, 235)',
+                    borderColor: 'rgba(54, 162, 235, 0.2)',
+                    yAxisID: 'y-axis-2',
+                },
+            ],
+        }
+
+        const multiLineOptions = {
+            scales: {
+                yAxes: [
+                    {
+                        type: 'linear',
+                        display: true,
+                        position: 'left',
+                        id: 'y-axis-1',
+                    },
+                    {
+                        type: 'linear',
+                        display: true,
+                        position: 'right',
+                        id: 'y-axis-2',
+                        gridLines: {
+                            drawOnArea: false,
+                        },
+                    },
+                ],
+            },
+        }
+        // multi line ends
+
+        // scatter
+        const rand = () => Math.round(Math.random() * 20 - 10)
+
+        const scatterData = {
+            datasets: [
+                {
+                    label: 'Product Hits',
+                    data: [
+                        {x: rand(), y: rand()},
+                        {x: rand(), y: rand()},
+                        {x: rand(), y: rand()},
+                        {x: rand(), y: rand()},
+                        {x: rand(), y: rand()},
+                        {x: rand(), y: rand()},
+                        {x: rand(), y: rand()},
+                        {x: rand(), y: rand()},
+                        {x: rand(), y: rand()},
+                        {x: rand(), y: rand()},
+                        {x: rand(), y: rand()},
+                        {x: rand(), y: rand()},
+                        {x: rand(), y: rand()},
+                        {x: rand(), y: rand()},
+                    ],
+                    backgroundColor: 'rgba(255, 99, 132, 1)',
+                },
+            ],
+        }
+
+        const scatterOptions = {
+            scales: {
+                yAxes: [
+                    {
+                        ticks: {
+                            beginAtZero: true,
+                        },
+                    },
+                ],
+            },
+        }
+        // scatter ends
+
+        //donut
+        const donutData = {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [
+                {
+                    label: 'Sales',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                    ],
+                    borderWidth: 1,
+                },
+            ],
+        }
+        // donut ends
 
         return (
             <div>
@@ -133,11 +278,22 @@ class Home extends Component {
                         <Col>
                             <div className="home-rectangle">
                                 <h4>Log Management</h4>
-                                <Line
-                                    data={logData}
-                                    height={100}
-                                    width={200}
-                                />
+                                <Row>
+                                    <Col>
+                                        <Line data={logData}/>
+                                    </Col>
+                                    <Col>
+                                        <Line data={lineData} options={lineOptions}/>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <Line data={multiLineData} options={multiLineOptions}/>
+                                    </Col>
+                                    <Col>
+                                        <Scatter data={scatterData} options={scatterOptions}/>
+                                    </Col>
+                                </Row>
                             </div>
                         </Col>
                     </Row>
@@ -146,11 +302,15 @@ class Home extends Component {
                         <Col>
                             <div className="home-rectangle">
                                 <h4>Business Analytics</h4>
-                                <Line
-                                    data={manageData}
-                                    height={50}
-                                    width={100}
-                                />
+                                <Row>
+                                    <Col>
+                                        <Doughnut data={donutData}/>
+                                    </Col>
+                                    <Col>
+                                        <Line data={manageData}/>
+                                    </Col>
+                                </Row>
+
                             </div>
                         </Col>
                     </Row>
