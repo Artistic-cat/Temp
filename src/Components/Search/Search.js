d0ead16a4a9f46efed86dac68d1fa761e7339159
Vidhi Sriavastava <srivastavavidhi87@gmail.com>
2020-11-19 12:03:33 +0530

    About code refactor

---
+++ src/Components/Search/Search.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Search = () => {
+    return (
+        <>
+        <div className = "searchbar">
+          <input type = "text" palceholder = "Search keyword" />
+        </div>
+        </>
+    );
+}
+
+export default Search;
