7f64cdd7485eecd3b37fa789fe50ea15cf1aaf52
Mohammed Rifas S <rifas2475@outlook.com>
2020-11-20 19:38:20 +0530

    File upload refactor
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- src/Views/FileUpload/FileUpload.js
+++ src/Views/FileUpload/FileUpload.js
@@ -3,22 +3,25 @@ import "./FileUpload.css";
 
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
-import {Container} from "react-bootstrap";
+import {Button, Container} from "react-bootstrap";
 import FileDropBox from "../../Components/FlieDropBox/FileDropBox";
-import Sidebar from "../../Components/Sidebar/Sidebar";
 
 class FileUpload extends Component {
     render() {
         return (
             <div>
                 <Header/>
-                <Sidebar/>
                 <Container>
                     <div className="file-file_box">
                         <h4>Upload Your Files to Proceed</h4>
 
                         <div className={"m-4"}>
                             <FileDropBox/>
+                        </div>
+                        <div className={"m-4"}>
+                            <Button>Back</Button>
+                            <Button>Cancel</Button>
+                            <Button>Next</Button>
                         </div>
                     </div>
                 </Container>
