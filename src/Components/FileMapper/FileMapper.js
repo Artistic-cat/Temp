a672ed80053d3f000e093c1c93adee7da7f7ea1e
Mohammed Rifas S <rifas2475@outlook.com>
2020-11-24 16:31:52 +0530

    File mapper
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- src/Components/FileMapper/FileMapper.js
+++ src/Components/FileMapper/FileMapper.js
@@ -1,67 +1,81 @@
 import React, {Component} from "react";
+import "./FileMapper.css";
 
-import Header from '../../Components/Header/Header';
-import Footer from '../../Components/Footer/Footer';
-import {Container, Table} from "react-bootstrap";
+import {Table} from "react-bootstrap";
 
 
 class FileMapper extends Component {
 
-    getFileList() {
-        return {
-            data: [
-                {
-                    name: "City Master",
-                    type: "csv",
-                    category: null
-                },
-                {
-                    name: "Pin-code Master",
-                    type: "xls",
-                    category: null
-                },
-                {
-                    name: "API Help File",
-                    type: "pdf",
-                    category: null
-                },
-                {
-                    name: "API File",
-                    type: "json",
-                    category: null
-                }
-            ]
-        };
+    getFileList = {
+        data: [
+            {
+                id: 1,
+                name: "City Master",
+                type: "csv",
+                category: null
+            },
+            {
+                id: 2,
+                name: "Pin-code Master",
+                type: "xls",
+                category: null
+            },
+            {
+                id: 3,
+                name: "API Help File",
+                type: "pdf",
+                category: null
+            },
+            {
+                id: 4,
+                name: "API File",
+                type: "json",
+                category: null
+            }
+        ]
+    };
+
+    onChangeValue(event) {
+        console.log(event.target.value);
+        this.getFileList.data.find(item => item.name).category = event.target.value;
+        console.log(this.getFileList)
     }
 
     render() {
         return (
-            <div>
-                <Header/>
-                <Container>
-                    <Table className="mapper-table" responsive hover>
-                        <thead>
+            <Table className="mapper-table" responsive hover>
+                <thead>
+                <tr>
+                    <td>File Name</td>
+                    <td>File Type</td>
+                    <td>File Category</td>
+                    <td>Remove File</td>
+                </tr>
+                </thead>
+                {
+                    [...this.getFileList.data].map((data) =>
                         <tr>
-                            <td>File Name</td>
-                            <td>File Type</td>
-                            <td>File Category</td>
-                            <td>Remove File</td>
+                            <td>{data.name}</td>
+                            <td>{data.type}</td>
+                            <td>
+                                <div onChange={(event) => {
+                                    this.getFileList.data.find(id => id.id === data.id).category = event.target.value;
+                                    console.log(this.getFileList.data)
+                                }
+                                }>
+                                    <input style={{marginLeft: '5%'}} type="radio" value="api_file"
+                                           name="file_type"/> API File
+                                    <input style={{marginLeft: '5%'}} type="radio" value="master_file"
+                                           name="file_type"/> Master File
+                                    <input style={{marginLeft: '5%'}} type="radio" value="help_file"
+                                           name="file_type"/> Help File
+                                </div>
+                            </td>
+                            <td style={{color: "red"}}>&#10060;</td>
                         </tr>
-                        </thead>
-                        {
-                            [...this.getFileList().data].map((data) =>
-                                <tr>
-                                    <td>{data.name}</td>
-                                    <td>{data.type}</td>
-                                    <td>{data.category}</td>
-                                    <td style={{color: "red"}}>&#10060;</td>
-                                </tr>
-                            )
-                        }
-                    </Table>
-                </Container>
-                <Footer/>
-            </div>
+                    )
+                }
+            </Table>
         )
     }
 }
