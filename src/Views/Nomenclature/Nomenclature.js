11766240c36960f45ad1994a1b49d4ec1c2afc18
Monika <monica@flexicious.com>
2020-12-24 21:40:02 +0530

    Nomenclature Fixes

--- src/Views/Nomenclature/Nomenclature.js
+++ src/Views/Nomenclature/Nomenclature.js
@@ -1,50 +1,36 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./Nomenclature.css";
 import axios from "axios";
-import {Container, Table} from "react-bootstrap";
+import { Container, Table } from "react-bootstrap";
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
 import { Link } from "react-router-dom";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import { Checkbox } from "@material-ui/core";
 
 class Nomenclature extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         isCheckboxChecked: false
-    //     };
-    // }
-
-    // handleOnCheckboxChange = (event, type) => {
-    //     switch (type) {
-    //         case "mmm":
-    //             if (event.target.checked) {
-    //                 this.setState({ isCheckboxChecked: true });
-    //             } else {
-    //                 this.setState({ isCheckboxChecked: false });
-    //             }
-    //             break;
-    //         default:
-    //             break;
-    //     }
-    // }
-
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            checkboxCheckedKey: [],
+            nomenclatureFileList: []
+        };
+    }
 
     async componentDidMount() {
-        try{
-            await axios.get(global.config.backend_ip 
-                +"/mastermap?insurerProductTransactionTypeId=156")
-               .then((response) =>{
+        try {
+            await axios.get(global.config.backend_ip
+                + "/mastermap?insurerProductTransactionTypeId=156")
+                .then((response) => {
                     this.setState({
                         nomenclatureFileList: response.data.apiResponse.data
                     })
                 })
-        
-            await axios.get(global.config.backend_ip 
-                +"/giixMasterMapper?insurerProductTransactionTypeId=156")
-                .then((response) =>{
+
+            await axios.get(global.config.backend_ip
+                + "/giixMasterMapper?insurerProductTransactionTypeId=156")
+                .then((response) => {
                     this.setState({
                         nomenclatureList: response.data.apiResponse.data
                     })
@@ -53,16 +39,29 @@ class Nomenclature extends Component {
         catch (error) {
             console.log(error.response);
             alert("An error occurred, please try again.");
-            console.log({error});
+            console.log({ error });
+        }
+    }
+
+    handleOnCheckboxChange = (event) => {
+        const { checkboxCheckedKey } = this.state;
+        if (event.target.checked) {
+            let checkedKeys = checkboxCheckedKey;
+            checkedKeys.push(event.target.name);
+            this.setState({ checkboxCheckedKey: checkedKeys })
+        }
+        else {
+            // this.setState({ checkboxCheckedKey: "" })
+            // checkedKeys =  checkboxCheckedKey.filter data !== event.target.name
         }
     }
 
     render() {
-        // const { isCheckboxChecked } = this.state;
-        return(
+        const { checkboxCheckedKey, nomenclatureFileList } = this.state;
+        return (
             <>
-               <Header/>
-               <div className='completed-status-bar'>
+                <Header />
+                <div className='completed-status-bar'>
                     <div className='left-name'>Set Up Master</div>
                     <div className='status-icons'>
                         <div className='status-icon-complete'></div>
@@ -71,11 +70,11 @@ class Nomenclature extends Component {
                         <div className='status-icon-incomplete'></div>
                     </div>
                 </div>
-                <br/>
-                <Container className = "container-box">
-                    <div style={{maxHeight:320,minHeight:50}}>
-                            <Table className = "mapper-table" responsive striped hover  style={{minWidth:650}}>
-                            <thead className = "headings" style = {{fontSize : "2.5vh"}}>
+                <br />
+                <Container className="container-box">
+                    <div style={{ maxHeight: 320, minHeight: 50 }}>
+                        <Table className="mapper-table" responsive striped hover style={{ minWidth: 650 }}>
+                            <thead className="headings" style={{ fontSize: "2.5vh" }}>
                                 <tr>
                                     <td>Giix Standard Masters</td>
                                     <td>Insurer Master Not Available</td>
@@ -86,39 +85,42 @@ class Nomenclature extends Component {
                                 </tr>
                             </thead>
                             <tbody>
-                                {
-                                    (this.state != null?
-                                        [...this.state.nomenclatureFileList].map((data, key)=>
+                                {nomenclatureFileList ?
+                                    [...(nomenclatureFileList || [])].map((data, key) =>
                                         <tr key={data.id}>
                                             <td>{data.name}</td>
                                             {/*// TODO: Set the appropriate Data in the status of master dependency file field*/}
-                                            <td><input 
-                                                    type="checkbox"    
-                                                    className = "check" 
-                                                    value="2"/>
+                                            <td>
+                                                <Checkbox checked={checkboxCheckedKey.find((cck) => cck.toString() === key.toString())} onChange={this.handleOnCheckboxChange} name={key} />
                                             </td>
-                                                {/*// TODO: Set the status of the file according to the response*/}
-                                            <td><input 
-                                                   type = "text" 
-                                                   className = "txt"/>  
+                                            {/*// TODO: Set the status of the file according to the response*/}
+                                            <td>
+                                                <input
+                                                    type="text"
+                                                    className="txt"
+                                                    disabled={checkboxCheckedKey.find((cck) => cck.toString() === key.toString())}
+                                                />
                                             </td>
                                             {/*// TODO: Enable only one based on status*/}
-                                            <td><button className="button-select" onClick>Master file Selection</button></td>
-                                            <td><input type = "text" className = "txt"></input></td>
-                                            <td><Link style={{color:"blue"}}>Preview</Link></td>
-                                                
+                                            <td>
+                                                <button className="button-select">Master file Selection</button>
+                                            </td>
+                                            <td>
+                                                <input value={key} type="text" className="txt" />
+                                            </td>
+                                            <td>
+                                                <Link style={{ color: "blue" }}>Preview</Link>
+                                            </td>
                                         </tr>
-                                        
-                                        )
-                                    :"")
+                                    ) : <tr></tr>
                                 }
                             </tbody>
                         </Table>
                     </div>
 
-                    <div style={{maxHeight:350,minHeight:50,marginTop : "2%"}}>
-                        <Table  className = "mapper-table"  style={{minWidth:650}} responsive striped hover>
-                            <thead className = "headings" style = {{fontSize : "2.5vh"}}>
+                    <div style={{ maxHeight: 350, minHeight: 50, marginTop: "2%" }}>
+                        <Table className="mapper-table" style={{ minWidth: 650 }} responsive striped hover>
+                            <thead className="headings" style={{ fontSize: "2.5vh" }}>
                                 <tr>
                                     <td>Insurer Specific Master</td>
                                     <td>Generic Name</td>
@@ -131,43 +133,42 @@ class Nomenclature extends Component {
 
                             <tbody>
                                 {
-                                    (this.state !=null?
-                                        [...this.state.nomenclatureFileList].map((data, key)=>
-                                        <tr key={data.id}>
-                                            <td><input type = "text" className = "txt"></input></td>
-                                            {/*// TODO: Set the appropriate Data in the status of master dependency file field*/}
-                                            <td><input type = "text" className = "txt"></input></td>
+                                    (this.state != null ?
+                                        [...this.state.nomenclatureFileList].map((data, key) =>
+                                            <tr key={data.id}>
+                                                <td><input type="text" className="txt"></input></td>
+                                                {/*// TODO: Set the appropriate Data in the status of master dependency file field*/}
+                                                <td><input type="text" className="txt"></input></td>
                                                 {/*// TODO: Set the status of the file according to the response*/}
-                                            <td><input type = "text" className = "txt"></input></td>
-                                            {/*// TODO: Enable only one based on status*/}
-                                            <td><button className="button-select" onClick>Master file Selection</button></td>
-                                            <td><input type = "text" className = "txt"></input></td>
-                                            <td><Link style={{color:"blue"}}>Preview</Link></td>
-                                                
-                                        </tr>
-                                        
+                                                <td><input type="text" className="txt"></input></td>
+                                                {/*// TODO: Enable only one based on status*/}
+                                                <td><button className="button-select" onClick>Master file Selection</button></td>
+                                                <td><input type="text" className="txt"></input></td>
+                                                <td><Link style={{ color: "blue" }}>Preview</Link></td>
+
+                                            </tr>
+
                                         )
-                                    :"")
+                                        : "")
                                 }
                             </tbody>
                         </Table>
                     </div>
-                    <br/>
-                    <br/>
+                    <br />
+                    <br />
                     <Row className='navigation-buttons'>
-                            <Col><Link to='/mapfile'><button className='btn '>Back</button></Link></Col>
-                            <Col><Link><button className='btn '>Save as Draft</button></Link></Col>
-                            <Col><Link to='/status'><button className='btn ' style = {{width : "20vw"}}>Complete Master Nomenclature</button></Link></Col>
+                        <Col><Link to='/mapfile'><button className='btn '>Back</button></Link></Col>
+                        <Col><Link><button className='btn '>Save as Draft</button></Link></Col>
+                        <Col><Link to='/status'><button className='btn ' style={{ width: "20vw" }}>Complete Master Nomenclature</button></Link></Col>
                     </Row>
-                   </Container>
-                   <Footer/>
+                </Container>
+                <Footer />
             </>
-            
+
         )
     }
 }
 
-    export default Nomenclature;
+export default Nomenclature;
 
-   
-      
+
